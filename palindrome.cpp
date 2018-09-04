d3cf9cbf3a6bf15ce61d26c5a6cbac16c5fdff80
nitish kumar <nk17kumar@gmail.com>
2018-09-04 17:17:15 +0530

    added palindrome file

---
+++ palindrome.cpp
@@ -0,0 +1,13 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    string s;
+    cin >> s;
+    int l = s.length();
+
+    cout << isPalindrome(s) << endl;
+
+    return 0;
+}
